1f3c3795a7346a906a68a43e1a91e801ed2f7fb1
ppunnura (Khaow) <67050359@kmitl.ac.th>
2026-02-03 20:23:57 +0700

    Create background.js

---
+++ cell03/ex00/background.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  document.body.style.backgroundColor =
+    "rgb(" + r + "," + g + "," + b + ")";
+}
