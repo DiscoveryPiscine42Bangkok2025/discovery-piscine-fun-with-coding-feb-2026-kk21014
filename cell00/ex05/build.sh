da46932b5d5371007ce8d0878bb3aa4e4028e655
ppunnura (Khaow) <67050359@kmitl.ac.th>
2026-02-03 10:16:42 +0700

    Update build.sh

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,1 +1,24 @@
+cd ~
+cd discovery_piscine
+cd cell00
+mkdir ex05
+cd ex05
+touch build.sh
+code build.sh
+#!/bin/bash
 
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied"
+else
+  for arg in "$@"
+  do
+    mkdir "ex$arg"
+  done
+fi
+
+
+chmod +x build.sh
+./build.sh
+./build.sh 00
+./build.sh 01 02 03 04
+ls
