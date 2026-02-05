25240b138601f74893743508b3f9f19f711ddbd9
ppunnura (Khaow) <67050359@kmitl.ac.th>
2026-02-05 15:35:47 +0700

    Update count_files.sh

--- cell00/ex03/count_files.sh
+++ cell00/ex03/count_files.sh
@@ -1,11 +1,2 @@
-cd ~
-cd discovery_piscine
-cd cell00
-mkdir ex03
-cd ex03
-touch count_files.sh
-code count_files.sh
 #!/bin/bash
 ls -l | grep -v '^total' | wc -l
-chmod +x count_files.sh
-./count_files.sh
